9ea90ef0d1ce51eb7a8374baa82c7073e96d529c
Chad Button <cbutton@freedomdebtrelief.com>
2023-07-16 06:39:41 -0700

    ui setup

---
+++ source/webui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "webuiba010288": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "ressy": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
